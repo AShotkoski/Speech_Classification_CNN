e4bc1b48e0806504fe51e3862f8872bdc517d295
AShotkoski <Shotkoskialex@gmail.com>
2026-03-22 17:20:19 -0600

    renamed datapipeline to match its function

--- Src/test_datapipeline.py
+++ Src/test_datapipeline.py
@@ -1,9 +1,9 @@
 import os
-import datapipeline
+import LibriSpeechData
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-ds = datapipeline.LibriSpeechWordDataset(
+ds = LibriSpeechData.LibriSpeechWordDataset(
     root = os.path.join(script_dir, "..\\LibriSpeech"),
     splits = ["train-clean-100"],
     top_k = 1000,
