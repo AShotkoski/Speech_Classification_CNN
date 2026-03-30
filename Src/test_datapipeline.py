ccc92c1423da5cca7daafe3d1f8b3781a7547d28
alex <shotkoskialex@gmail.com>
2026-03-30 11:00:39 -0600

    added test code to implement database

--- Src/test_datapipeline.py
+++ Src/test_datapipeline.py
@@ -1,12 +1,20 @@
 import os
 import LibriSpeechData
+from torch.utils.data import DataLoader
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 ds = LibriSpeechData.LibriSpeechWordDataset(
     root = os.path.join(script_dir, "..\\LibriSpeech"),
-    splits = ["train-clean-100"],
+    splits = ["dev-clean"],
     top_k = 1000,
 )
+
+loader = DataLoader(ds, batch_size=4, shuffle=True, collate_fn=LibriSpeechData.collate_fn)
+
+features = next(iter(loader))
+print("Features shape:", features[0].shape)
+print("Labels shape:", features[1].shape)
+print(ds[features[1][1]])
 
 print(f"Dataset size: {len(ds)}")
