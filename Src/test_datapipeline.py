0af7740a5c3ed9578221ce6c63b2cfba2c6e2d3c
alex <shotkoskialex@gmail.com>
2026-03-30 11:55:03 -0600

    testing the mel spectrograms

--- Src/test_datapipeline.py
+++ Src/test_datapipeline.py
@@ -1,20 +1,37 @@
 import os
 import LibriSpeechData
 from torch.utils.data import DataLoader
+from torchaudio.transforms import AmplitudeToDB
+import matplotlib.pyplot as plt
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 ds = LibriSpeechData.LibriSpeechWordDataset(
     root = os.path.join(script_dir, "..\\LibriSpeech"),
     splits = ["dev-clean"],
-    top_k = 1000,
+    top_k = 100
 )
 
-loader = DataLoader(ds, batch_size=4, shuffle=True, collate_fn=LibriSpeechData.collate_fn)
+#loader = DataLoader(ds, batch_size=4, shuffle=True, collate_fn=LibriSpeechData.collate_fn)
 
-features = next(iter(loader))
-print("Features shape:", features[0].shape)
-print("Labels shape:", features[1].shape)
-print(ds[features[1][1]])
+#features, labels, lengths = next(iter(loader))
+#print("Features shape:", features.shape)
+#print("Labels shape:", labels.shape)
+#print("lengths: ", lengths)
 
 print(f"Dataset size: {len(ds)}")
+
+mel_spec1, label1 = ds[-1]
+mel_spec2, label2 = ds[-5]
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 4))
+
+
+im1 = axes[0].imshow(mel_spec1, origin='lower', aspect='auto', cmap='magma')
+axes[0].set_title('Mel Spectrogram (ds[-1])')
+
+im2 = axes[1].imshow(mel_spec2, origin='lower', aspect='auto', cmap='magma')
+axes[1].set_title('Mel Spectrogram (ds[-5])')
+
+plt.show()
+
