05e19aa3582dbc1f962dbab1261b17b97b7fb652
AShotkoski <Shotkoskialex@gmail.com>
2026-03-31 18:43:35 -0600

    renamed librispeechdata to librispeechdataset to reflect the accurate name

--- Src/test_datapipeline.py
+++ Src/test_datapipeline.py
@@ -1,18 +1,18 @@
 import os
-import LibriSpeechData
+import LibriSpeechDataset
 from torch.utils.data import DataLoader
 from torchaudio.transforms import AmplitudeToDB
 import matplotlib.pyplot as plt
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-ds = LibriSpeechData.LibriSpeechWordDataset(
+ds = LibriSpeechDataset.LibriSpeechWordDataset(
     root = os.path.join(script_dir, "..\\LibriSpeech"),
     splits = ["dev-clean"],
     top_k = 100
 )
 
-loader = DataLoader(ds, batch_size=4, shuffle=True, collate_fn=LibriSpeechData.collate_fn)
+loader = DataLoader(ds, batch_size=4, shuffle=True, collate_fn=LibriSpeechDataset.collate_fn)
 
 features, labels = next(iter(loader))
 print("Features shape:", features.shape)
